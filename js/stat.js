e9ca1b3149593f90989dea5ca18e9991f63e6bba
SergeiBychkov <sergeysteers@gmail.com>
2020-01-25 17:43:35 +0300

    добавляет облачко

---
+++ js/stat.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var CLOUD_TEXT_X = 140;
+var GAP = 10;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
+var BAR_X = 140;
+var BAR_Y = 95;
+var BAR_GAP = 50;
+
+var renderCloud = function(ctx, x, y, color) {
+ctx.fillStyle = color;
+ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function(arr) {
+    var maxElement = arr[0];
+    
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > maxElement) {
+        maxElement = arr[i];
+      }
+    }
+    
+    return maxElement;
+  };
+
+window.renderStatistics = function(ctx, names, times) {
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+    ctx.font = '16px PT Mono';
+    ctx.textBaseline = 'hanging';
+    ctx.fillStyle = '#000';
+    ctx.fillText('Ура вы победили!', CLOUD_X + GAP, 35);
+    ctx.fillText('Список результатов:', CLOUD_X + GAP, 55);
+
+    var maxTime = getMaxElement(times);
+
+    for (var i = 0; i < names.length; i++) {
+        ctx.fillStyle = '#000';
+        ctx.fillText(Math.round(times[i]), CLOUD_TEXT_X + (BAR_WIDTH + BAR_GAP) * i, 80);
+        ctx.fillText(names[i], CLOUD_TEXT_X + (BAR_WIDTH + BAR_GAP) * i, 260);
+        if (names[i] === 'Вы') {
+            ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+        }
+        else {
+            ctx.fillStyle = 'hsl(240, 100%, 50%)';
+        }
+        ctx.setTransform(1, 0, 0, -1, 0, 350);
+        ctx.fillRect(BAR_X + (BAR_WIDTH + BAR_GAP) * i, BAR_Y, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+    }
+};
+
+/*
+рандомайз
+var getRandomInt = function (min, max) {
+  min = Math.ceil(25);
+  max = Math.floor(75);
+  return Math.floor(Math.random() * (75 - 25)) + 25 + '%';
+}
+
+console.log(getRandomInt());
+*/
